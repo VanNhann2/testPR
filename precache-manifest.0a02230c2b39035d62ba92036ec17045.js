3d8bbf1868ef8d23496c998de2d62dd0f2e169f5
vannhan <nhanitdhsp@gmail.com>
2019-03-19 20:28:25 +0700

    test.oi

---
+++ precache-manifest.0a02230c2b39035d62ba92036ec17045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2ed2aeb48ed292d60a5",
+    "url": "/static/css/main.e1669466.chunk.css"
+  },
+  {
+    "revision": "f2ed2aeb48ed292d60a5",
+    "url": "/static/js/main.f2ed2aeb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "043e0a54ca8846d840bc",
+    "url": "/static/js/2.043e0a54.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "f51102119153d852e794319bf6d59dde",
+    "url": "/index.html"
+  }
+];
